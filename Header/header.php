2ca22add5daf56315e6e3ecab1d0d3bdb4ec26fd
themetalcrackhead <divyanshu8sharma@gmail.com>
2014-03-14 01:47:52 +0530

    CHAIRPERSON

--- Header/header.php
+++ Header/header.php
@@ -18,6 +18,7 @@
 					<a class="codrops-icon codrops-icon-drop" href=""><span>SignUp/Login</span></a>
 				</span>
 			</div>
+			<br>
 			<header class="codrops-header">
 				<h1 style="margin:-11% 0 12% 0;">
 					<img src="header/img/BVPIEEE.png" style="float:left; margin-left:1;" width="450">
@@ -33,17 +34,17 @@
 		<div class="overlay overlay-scale">
 			<button type="button" class="overlay-close">Close</button>
 			<aside align="center" style="margin-top:5%;">
-			    <p><font color="black" face="calibri" size="5"><b>Note from the Chairperson</b></font></p>
+			    <p><font color="black" face="calibri" size="5.5"><b>Note from the Chairperson</b></font></p>
 			    <br>
 			    <img alt="" src="header/img/haresh.png" border="50" height="150" width="150"><br><br>
-			    <p><font color="black" face="calibri" size="4"><b>Haresh Singh<br>(Chairperson)</b></font></p>
+			    <p><font color="black" face="calibri" size="4"><b>Haresh Singh</b></font></p>
 			</aside>
 			<nav style="margin-top:-9%;">
 				<ul>
 					<li>
-					<p><font color="#504A4B" face="calibri" size="4">Welcome to the website of the IEEE Student Branch of Bharati Vidyapeeth&rsquo;s College of Engineering, BVPIEEE. Institute of Electrical and Electronics Engineers (IEEE) is the world&rsquo;s largest professional association dedicated to advancing technological innovation and excellence for the benefit of humanity. We at BVPIEEE have always promoted IEEE as a common platform to share knowledge through interactive sessions, seminars, field trips, conferences and have stayed true to the essence of IEEE. Our aim is to create a talent pool where students interact and exchange ideas, network with people from various institutions and disciplines, and most importantly, learn in the process.</p>
-				    <p>This website gives you a look into the activities we have undertaken at the BVCOE Student Branch. We have also listed the various SIGs (Special Interest Groups) that we have started. It also gives a sneak peek into our premier events- Our annual technical and managerial gest, Fervour and Dr. Vikram Sarabhai Rotating Shield Quiz. Photographs and videos of the events have also been uploaded on the website to give a better visual experience. We conduct various events all throughout the academic session and encourage you to participate in them and give us your valuable feedback. Please visit our website regularly to get a glance into our chapter and learn about the upcoming events.</p>
-				    <p>I encourage you to become a member of this global community, IEEE and reap the benefits that it offers as a professional organization. You can highly benefit from the plethora of knowledge and technical assistance it offers. For any further queries, feel free to contact us. We look forward to a positive response in the near future and hope that your association with BVPIEEE will be an enriching experience.</font></p>
+					<p><font color="#504A4B" face="calibri" size="4.7">Welcome to the website of the IEEE Student Branch of Bharati Vidyapeeth&rsquo;s College of Engineering, BVPIEEE. Institute of Electrical and Electronics Engineers (IEEE) is the world&rsquo;s largest professional association dedicated to advancing technological innovation and excellence for the benefit of humanity. We at BVPIEEE have always promoted IEEE as a common platform to share knowledge through interactive sessions, seminars, field trips, conferences and have stayed true to the essence of IEEE. Our aim is to create a talent pool where students interact and exchange ideas, network with people from various institutions and disciplines, and most importantly, learn in the process.</p>
+				    <br><p>This website gives you a look into the activities we have undertaken at the BVCOE Student Branch. We have also listed the various SIGs (Special Interest Groups) that we have started. It also gives a sneak peek into our premier events- Our annual technical and managerial gest, Fervour and Dr. Vikram Sarabhai Rotating Shield Quiz. Photographs and videos of the events have also been uploaded on the website to give a better visual experience. We conduct various events all throughout the academic session and encourage you to participate in them and give us your valuable feedback. Please visit our website regularly to get a glance into our chapter and learn about the upcoming events.</p>
+				    <br><p>I encourage you to become a member of this global community, IEEE and reap the benefits that it offers as a professional organization. You can highly benefit from the plethora of knowledge and technical assistance it offers. For any further queries, feel free to contact us. We look forward to a positive response in the near future and hope that your association with BVPIEEE will be an enriching experience.</font></p>
 					</li>
 				</ul>
 			</nav>

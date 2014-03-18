413a8b26416cb8e9835a7ccbe465e601a90de4be
archangel <adi.shr09@gmail.com>
2014-03-18 23:55:38 +0530

    footer slider goone

--- index.php
+++ index.php
@@ -145,17 +145,6 @@
         </ul>
 		<p style="float:right; display:inline;">Keep In Touch
 		</p>
-		<div id="slideshow">
-		<div>
-			<img src="http://farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg">
-		</div>
-		<div>
-			<img src="http://farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg">
-		</div>
-		<div>
-			Pretty cool eh? This slide is proof the content can be anything.
-		</div>
-		</div>
 	</div>
 </div>
 <script>

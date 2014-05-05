fabcad1e18c8d4a37447a86c90ebcb7913040af9
kunal95 <kunsachdeva@gmail.com>
2014-05-05 12:38:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,154 +1,238 @@
-<html style="overflow-x:hidden;">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1""> 
-		<title>BVPIEEE | Passion To Perfection</title>
-		<link rel="stylesheet" href="css/body.css" type="text/css">
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-		<script src="js/jquery.fittext.js"></script>
-		<script>
-			jQuery("#responsive_headline").fitText();
-		</script>
-	</head>
-<?php include ("Header/header.php"); ?>
-<audio loop autoplay>
-  <source src="Oooh.mp3" type="audio/mpeg">
-</audio>
-<script>
-			//  The function to change the class
-			var changeClass = function (r,className1,className2) {
-				var regex = new RegExp("(?:^|\\s+)" + className1 + "(?:\\s+|$)");
-				if( regex.test(r.className) ) {
-					r.className = r.className.replace(regex,' '+className2+' ');
-			    }
-			    else{
-					r.className = r.className.replace(new RegExp("(?:^|\\s+)" + className2 + "(?:\\s+|$)"),' '+className1+' ');
-			    }
-			    return r.className;
-			};	
+<!DOCTYPE html>
 
-			//  Creating our button in JS for smaller screens
-			var menuElements = document.getElementById('menu');
-			menuElements.insertAdjacentHTML('afterBegin','<button type="button" id="menutoggle" class="navtoogle" aria-hidden="true"><i aria-hidden="true" class="icon-menu"> </i> Menu</button>');
+<html lang="en" class="no-js wf-adobecaslonpro-i4-active wf-adobecaslonpro-i7-active wf-adobecaslonpro-n4-active wf-adobecaslonpro-n7-active wf-futurapt-n3
 
-			//  Toggle the class on click to show / hide the menu
-			document.getElementById('menutoggle').onclick = function() {
-				changeClass(this, 'navtoogle active', 'navtoogle');
-			}
+-active wf-futurapt-n4-active wf-active"><!--<![endif]-->
+<head><script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/app.css" type="text/css" media="screen">
+		<link rel="stylesheet" type="text/css" media="print" href="css/print.css">
+		<link rel="stylesheet" href="css/core.css"> 
+		<script src="js/app.js"></script>		<script src="js/ticker.js"></script>
+		<link rel="stylesheet" href="flux_files/css3menu2/style.css" type="text/css" /><style type="text/css">._css3m{display:none}</style>
+<script type="text/javascript">
+		$(function(){
+			$('#vertical-ticker').totemticker({
+					});
+	</script>
 
-			// http://tympanus.net/codrops/2013/05/08/responsive-retina-ready-menu/comment-page-2/#comment-438918
-			document.onclick = function(e) {
-				var mobileButton = document.getElementById('menutoggle'),
-					buttonStyle =  mobileButton.currentStyle ? mobileButton.currentStyle.display : getComputedStyle(mobileButton, null).display;
+		<style type="text/css">.tk-futura-pt{font-family:"futura-pt","Arial",sans-serif;}.tk-adobe-caslon-pro{font-family:"adobe-caslon-
 
-				if(buttonStyle === 'block' && e.target !== mobileButton && new RegExp(' ' + 'active' + ' ').test(' ' + mobileButton.className + ' ')) {
-					changeClass(mobileButton, 'navtoogle active', 'navtoogle');
-				}
-			}
-		</script>
-<?php include ("Nav_Bar/nav_bar.php"); ?>
-<?php include ("Slider/index2.php"); ?>
-<script type="text/javascript">	
-			$(function() {
+pro","Georgia",serif;}</style><link rel="stylesheet" href="http://use.typekit.com/c/3f00dc/adobe-caslon-pro:i4:i7:n4:n7,futura-
+
+pt:n3:n4.XWq:N:2,XWp:N:2,XWr:N:2,XWn:N:2,SHB:N:2,Y5K:N:2/d?
+
+3bb2a6e53c9684ffdc9a99f71d5b2a62d9c86262f6f33f54f782cc3094d002f08137af0598650f990161e1e48ed58c99dc446453b2e30031bbd93bdfb6c9f6b0c65d4fe6fe89959d353207eaab29
+
+05093c76263d2671fa5a7483d25ef230662e4041d3cd9b67e0c8bfdb2b02c78695b6abd5e2642d39714ff24b70e4a140e364a29d6d862b0aeff49ddfce4cddc3044d30502ed2fd0101879295c8ae
+
+09883ba4b83cc0414089b087b014e78a2a0ec763fd7c0a906df5f109d8191730b4b69f9864dd0c08608aab620f6b1fffd9e4ef19db88bb8b4d9b40b354fb786a141a55de3ae1d0eefddb51573a7e
+
+12b6533ee8b8862bae5ef0625c93fd071cc20a5b35610e2f5ff10a9d7b084b00ec53ac19778bc7f4bc1c81ea0ff7427b9bad2cb13805151474d2404212eb0a10ecf5017f7f22bb349e3d50eea960
+
+b9a3c85016"><script type="text/javascript">try{Typekit.load({ active: fontsReady });}catch(e){}</script>
+	<!--<script type="text/javascript" src="https://s3.amazonaws.com/ha_js/ga_social_tracking.js"></script>-->
+		<script src="js/flux.min.js" type="text/javascript" charset="utf-8"></script>
+
+	<title>bvpieee</title>
+	<script type="text/javascript" charset="utf-8" src="Notifications_edgePreload.js"></script>
+    <style>
+        .edgeLoad-Notifications { visibility:hidden; }
+    </style>
+
+</head>
+	<body class="home">
+			<header class="main">
+			<div id="tweets" class="revolver">
+				<div class="wrap barrel">
+				  <aside>
+				    <p><font color="black" face="impact">Note from the Chairperson</font></p>
+				    <br>
+				    <img alt="" src="images/haresh.jpg" border="50"><br><br>
+				    <p>Haresh Singh<br>
+				      (Chairperson)</p>
+			      </aside>
+				<ul>
+				  <li>
+				    <p><strong>Welcome to the website of the IEEE Student Branch of Bharati Vidyapeeth&rsquo;s College of Engineering, 
+
+BVPIEEE. Institute of Electrical and Electronics Engineers (IEEE) is the world&rsquo;s largest professional association dedicated to advancing technological 
+
+innovation and excellence for the benefit of humanity. We at BVPIEEE have always promoted IEEE as a common platform to share knowledge through interactive 
+
+sessions, seminars, field trips, conferences and have stayed true to the essence of IEEE. Our aim is to create a talent pool where students interact and 
+
+exchange ideas, network with people from various institutions and disciplines, and most importantly, learn in the process.</strong></p>
+				    <p><strong>This website gives you a look into the activities we have undertaken at the BVCOE Student Branch. We have 
+
+also listed the various SIGs (Special Interest Groups) that we have started. It also gives a sneak peek into our premier events- Our annual technical and 
+
+managerial gest, Fervour and Dr. Vikram Sarabhai Rotating Shield Quiz. Photographs and videos of the events have also been uploaded on the website to give a 
+
+better visual experience. We conduct various events all throughout the academic session and encourage you to participate in them and give us your valuable 
+
+feedback. Please visit our website regularly to get a glance into our chapter and learn about the upcoming events.</strong></p>
+				    <p><strong>I encourage you to become a member of this global community, IEEE and reap the benefits that it offers as a 
+
+professional organization. You can highly benefit from the plethora of knowledge and technical assistance it offers. For any further queries, feel free to 
+
+contact us. We look forward to a positive response in the near future and hope that your association with BVPIEEE will be an enriching 
+
+experience.</strong></p>
+			      </li>
+			  </ul>
+								</div>
+
+			</div>
+			<div class="wrap">
+				<nav class="consistent">
+				  <div align="center">
+				    <ul>
+				      <li><a href="webmail.bvpieee.com">Login</a></li>
+				      <li class="search">
+				        <a href=""><span></span>Search</a>
+			          </li>
+			        </ul>
+
+				    <ul>
+				      <li class="search"> </li>
+			        </ul>
+			      </div>
+				  <ul>
+				    <li class="search">
+				      <form method="post" action="http://www.bvpieee.com/search/">
+				        <div align="center">
+				          <input type="text" value="" name="q">
+				          <input class="submit" type="submit" value="">
+			            </div>
+				        </form>
+				      </li>
+				    </ul>
+			    </nav>
+				<div align="center"><a id="tweet_handle" href="http://www.bvpieee.com	/#"></a>
+			  </div>
+					<nav class="consistent-left">
+				  <div align="center">
+				  <font color="#999">
+				    <ul>
+				      <li class="first"><a class="topLink" href="http://www.ieee.org/">IEEE.org</a>&nbsp;</li>
+				      <li><a href="http://ieeexplore.ieee.org//">IEEE Xplore Digital Library</a></li>
+				      <li><a href="http://standards.ieee.org/">IEEE Standards</a></li>
+				      <li><a href="http://spectrum.ieee.org/">IEEE Spectrum</a></li>
+
+			        </ul>
+					</font>
+			      </div>
+			    </nav>
+                
+            </div>
+		</header>
+		<div class="body">
+		<div class="nav_bar">
+			<div id="logo">
+    
+      		  <div align="center"><img src="images/Bharatividyapeeth_logo.png" ><img src="images/BVPIEEE.png" width="400" ><img src="images/IEEE.png" 
+
+width="160" ></div>
+				<ul id="css3menu2" class="topmenu">
+
+	<li class="topfirst"><a href="index.php" style="height:19px;line-height:19px;"><img src="flux_files/css3menu2/home.png" alt=""/>HOME</a></li>
+
+	<li class="topmenu"><a href="ieee.php" style="height:19px;line-height:19px;">IEEE</a></li>
+
+	<li class="topmenu"><a href="bvpieee.php" style="height:19px;line-height:19px;">BVPIEEE</a></li>
+
+	<li class="topmenu"><a href="hkn.php" style="height:19px;line-height:19px;">IEEE-HKN</a></li>
+
+	<li class="topmenu"><a href="wie.php" style="height:19px;line-height:19px;">WIE</a></li>
+
+	<li class="topmenu"><a href="events.php" style="height:19px;line-height:19px;"><span>EVENTS</span></a>
+
+	<ul>
+
+		<li><a href="event2.php">EVENTS 2</a></li>
+
+		<li><a href="event1.php">EVENTS 1</a></li>
+
+		<li><a href="event0.php">EVENTS 0</a></li>
+
+	</ul></li>
+
+	<li class="topmenu"><a href="publications.php" style="height:19px;line-height:19px;"><span>PUBLICATIONS</span></a>
+
+	<ul>
+
+		<li><a href="reports.php">REPORTS</a></li>
+
+		<li><a href="ewh.ieee.org/sb/delhi/bvce/Pratibimb">PRATIBIMB</a></li>
+
+	</ul></li>
+
+	<li class="topmenu"><a href="sig.php" style="height:19px;line-height:19px;">SIGs</a></li>
+
+	<li class="topmenu"><a href="news.php" style="height:19px;line-height:19px;">NEWS</a></li>
+
+	<li class="topmenu"><a href="execomm.php" style="height:19px;line-height:19px;"><span>EXECUTIVE COMMUNITY</span></a>
+
+	<ul>
+
+		<li><a href="item10.php">Item 1 0</a></li>
+
+		<li><a href="item11.php">Item 1 1</a></li>
+
+		<li><a href="item12.php">Item 1 2</a></li>
+
+	</ul></li>
+
+	<li class="topmenu"><a href="gallery.php" style="height:19px;line-height:19px;">GALLERY</a></li>
+
+	<li class="toplast"><a href="contact.php" style="height:19px;line-height:19px;"><span>CONTACT US </span></a>
+
+	  <ul>
+
+	    <li><a href="collaborate.php">COLLABORATE</a></li>
+
+	    </ul></li>
+	</ul>
+	</div>
+	</div>
+	<div class = "body">
+		<div class = "center">
+				<p>&nbsp;</p>
+			  <p>&nbsp;</p> 
+               
+				<div id="qwert" >
+                 <div id="slider">
+               <img src="http://www.bsports.com/wp-content/uploads/2013/10/sellpage-bg-2000.jpg" alt="" style="width:100%!important;height:100%!important;"/>
+                <img src="http://percolate.com/media/images/brochure/home/hero_bg.jpg" alt="" style="width:100%!important;height:100%!important;"/>
+                <img src="http://www.westernwishes.org/wp-content/uploads/2012/12/bg-blue-stars.jpg" alt="" style="width:100%!important;height:100%!important;"/>
+            </div>
+              </div>
+		         
+                <div id="column_left">
+			<p> Div 1</p>
+			</div>
+					<div id="Stage" class="Notifications"></div>
+			<div id="column_right">
+<?php include 'right.php'; ?>
+
+			</div>
 			
-				var Page = (function() {
-
-					var $nav = $( '#nav-dots > span' ),
-						slitslider = $( '#slider' ).slitslider( {
-							onBeforeChange : function( slide, pos ) {
-
-								$nav.removeClass( 'nav-dot-current' );
-								$nav.eq( pos ).addClass( 'nav-dot-current' );
-
-							}
-						} ),
-
-						init = function() {
-
-							initEvents();
-							
-						},
-						initEvents = function() {
-
-							$nav.each( function( i ) {
-							
-								$( this ).on( 'click', function( event ) {
-									
-									var $dot = $( this );
-									
-									if( !slitslider.isActive() ) {
-
-										$nav.removeClass( 'nav-dot-current' );
-										$dot.addClass( 'nav-dot-current' );
-									
-									}
-									
-									slitslider.jump( i + 1 );
-									return false;
-								
-								} );
-								
-							} );
-
-						};
-
-						return { init : init };
-
-				})();
-
-				Page.init();
-
-				/**
-				 * Notes: 
-				 * 
-				 * example how to add items:
-				 */
-
-				/*
-				
-				var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
-				
-				// call the plugin's add method
-				ss.add($items);
-
-				*/
-			
-			});
-		</script>
-
-<div id="wrapper" >
-	<div id="container">
-		<div id="left">
-			<p>This is the left column
-			</p>
+<!-- Don't remove spacer div. Solve an issue about container height -->
+		<div class="spacer"></div>
+<!-- Don't remove spacer div. Solve an issue about container height -->
+		<div class="spacer"></div>
+er div. Solve an issue about container height -->
+		<div class="spacer"></div>
 		</div>
-		<div id="right">
 		
+		  <div id="footer">
+		<p>© 2013 BVPIEEE | Passion For Perfection</p>
 		</div>
-		<div id="right">
-			<p>This is 2nd right</p>
-		</div>
-		<div id="right">
-			<p>This is 3rd right</p>
-		</div>
-	</div>
-	<div id="left_f" style="border-radius:50px 50px 0 0;background:rgba(236, 236, 236, 1);">
-		<p style="float:left;">BVPIEEE | Passion To Perfection</p>
-		<p style="float:right; display:inline;">Keep In Touch
-		</p>
-	</div>
-</div>
-<script>
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() { 
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  3000);
-</script>
+          </div>
+      
+        </div>
+     
+</body>
 </html>

c1a8dc3d39ab24010fd0826f6bf3a2934cab0c21
kunal95 <kunsachdeva@gmail.com>
2014-05-05 12:45:53 +0530

    Create right.php

---
+++ right.php
@@ -0,0 +1,47 @@
+<?php
+echo '
+<div class="panel panel-default">
+            <div class="panel-body">
+                <ul id="vertical-ticker">
+                    <li>Send your resumes for Outstanding Young Professional Award; Vladimir Karapetoff Outstanding Technical Achievement Award; Distinguished Service.<br>DEADLINE: 30 April 2014<br>Send your resumes at</li>
+                    <li>Lambda Eta Chapter Won our second consecutive Outstansing Chapter Award.</li>
+                    <li>Lambda Eta Chapter came runners up in the HKN Video Contest.</li>
+                    
+                </ul><br>
+                <div>
+            
+            </div>
+                <br>
+                <div class="prez">
+                <div id="sliderd" style="margin: 2%;">
+                            <ul>
+                                <li class="current "><img src="img/akshay.png" style="max-width: 100%; height: 78px !important; width: 78px; float:left;">
+                                    <b>Akshay Gupta<br /> President 2011-12 <br /> <br /> </b>
+                                    <p>
+                                    Eta Kappa Nu has been dedicated to encouraging and recognizing excellence in Electrical and Computer Engineering. Not only does it honor scholarship, but also instills character, attitude and a sense of <a href="akshay.php" target="_blank">Continue>></a> 
+                                    </p>
+                                    </b>
+                                </li>
+                                <li class="current "><img src="img/vishal.png" style="max-width: 100%; height: 78px !important; width: 78px; float:left;">
+                                    <b>Vishal Madan<br /> President 2012-13 <br /> <br /> </b>
+                                    <p>
+                                   Lambda Eta Chapter, IEEE HKN, has showed an enormous potential and pursuit for excellence right from the beginning. Winning Outstanding Chapter Awards consecutively in the very first two years <a href="vishal.php" target="_blank">Continue>></a> 
+                                    </p>
+                                    </b>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                
+                </div>
+                <br>
+                <div>
+                <iframe src="https://www.google.com/calendar/embed?src=gios8q689lu3mms6ea455ml7mg%40group.calendar.google.com&ctz=Asia/Calcutta" style="border: 0" width="230" height="260" frameborder="0" scrolling="no"></iframe>
+            </div>
+            
+        </div>
+        
+    </div>
+
+';
+?>

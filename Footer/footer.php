c33cef73394513c8486b3154dd92ce96c9a81333
kunal95 <kunsachdeva@gmail.com>
2014-08-06 16:59:56 +0530

    Create footer.php

---
+++ Footer/footer.php
@@ -0,0 +1,28 @@
+<?php echo'
+
+    <style>
+        body{margin:0px;padding:0px;}
+        #footer{background-color:rgba(236, 236, 236, 1);height:200px;padding-top:30px;}
+        .col{width:31%;float:left;height:200px;padding:1%;overflow-wrap:break-word}
+    </style>
+   <div id="footer">
+        <div class="col" id="col1">
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a>
+        </div>
+        <div class="col" id="col2">
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a>
+        </div>
+        <div class="col" id="col3">
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a><br />
+            <a href="#">Link1</a>
+        </div>
+    </div>
+';?>
